e3aeb81fbb920de74561db1b4f24aa754ab1f096
this.aahin <aahin2001@gmail.com>
2025-11-10 07:00:48 +0530

    added route for accordion component

--- src/routes/approutes.jsx
+++ src/routes/approutes.jsx
@@ -1,13 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import HomePage from "../homepage";
 import ErrorPage from "../errorpage/errorpage";
+import Accordion from "../accordion-component/accordion";
 
 export default function Approutes() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
+      <Route path="project/accordion" element={<Accordion/>} />
       <Route path="*" element={<ErrorPage />} />
-      {/* <Route path="/project/:id" element={<ProjectPage />} /> */}
     </Routes>
   )
 }
