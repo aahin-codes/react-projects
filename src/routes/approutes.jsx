b590db0e46beb433beb5422d8acd2305e91339a8
this.aahin <aahin2001@gmail.com>
2025-11-12 07:30:55 +0530

    added route for random-color-generator component

--- src/routes/approutes.jsx
+++ src/routes/approutes.jsx
@@ -2,12 +2,14 @@ import { Routes, Route } from "react-router-dom";
 import HomePage from "../homepage";
 import ErrorPage from "../errorpage/errorpage";
 import Accordion from "../accordion-component/accordion";
+import RandomColorGenerator from "../random-color-generator-component/random-color-generator";
 
 export default function Approutes() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="project/accordion" element={<Accordion/>} />
+       <Route path="project/random-color-generator" element={<RandomColorGenerator/>} />
       <Route path="*" element={<ErrorPage />} />
     </Routes>
   )
