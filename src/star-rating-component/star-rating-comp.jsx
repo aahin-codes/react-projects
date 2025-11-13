9acef02d7371b18c5b0d04959a94b20803d771ef
this.aahin <aahin2001@gmail.com>
2025-11-13 23:13:33 +0530

    component update

--- src/star-rating-component/star-rating-comp.jsx
+++ src/star-rating-component/star-rating-comp.jsx
@@ -1,10 +1,8 @@
-
-import { IoLogoGithub } from "react-icons/io";
-import { FaArrowLeftLong, FaStar } from "react-icons/fa6";
+import {  FaStar } from "react-icons/fa6";
 import data from "./star-data";
-import { Link } from "react-router-dom";
 import { useState } from "react";
 import './_star-rating.scss';
+import Rendering from "../rendering-component/rendering";
 export default function StarRatingComp({numOfStars = 5}) {
 
     const [rating, setRating] = useState(0);
@@ -15,32 +13,10 @@ export default function StarRatingComp({numOfStars = 5}) {
     function handleMouseLeave(){
         setHover(rating);
     }
-    
-    return (
-        <div className="rendering-comp">
-            <div className="rendering-comp-wrapper">
-                <nav>
-                    <ul>
-                        <li>
-                            <Link to={"/"}>
-                                <FaArrowLeftLong /> Projects
-                            </Link>
-                        </li>
-                        <li>
-                            <IoLogoGithub className="github-icon" />
-                        </li>
-                    </ul>
-                </nav>
-                <div className="rendering-comp-content">
-                    
-                    {data.map((item) => {
 
-                        return (
-                            <div className="content" key={item.type}>
-                                <h2>{item.title}</h2>
-                                <p>{item.shortDescription}</p>
-                                <p className="sub-title">Demo</p>
-                                <div className="demo-box star-comp">
+    function renderingBox(){
+        return(
+             <div className="demo-box star-comp">
                                    {
                                     [...Array(numOfStars)].map((_,ind)=>{
                                         let stars = ind+1;
@@ -55,17 +31,11 @@ export default function StarRatingComp({numOfStars = 5}) {
                                     
                                    }
                                 </div>
-                                <p className="sub-title">Description</p>
-                                <p>{item.description}</p>
-                            </div>
-                        );
-                    }
-
-                    )}
-
-                </div>
-            </div>
-        </div>
+        )
+    }
+    
+    return (
+      <Rendering data={data} demoBox={renderingBox()}/>
 
     )
 }
