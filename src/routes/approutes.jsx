7adb23de2410d7e7870a9ebfd030b6fc13b9070c
this.aahin <aahin2001@gmail.com>
2025-11-13 23:38:15 +0530

    added route for star component

--- src/routes/approutes.jsx
+++ src/routes/approutes.jsx
@@ -3,6 +3,7 @@ import HomePage from "../homepage";
 import ErrorPage from "../errorpage/errorpage";
 import Accordion from "../accordion-component/accordion";
 import RandomColorGenerator from "../random-color-generator-component/random-color-generator";
+import StarRatingComp from "../star-rating-component/star-rating-comp";
 
 export default function Approutes() {
   return (
@@ -10,6 +11,7 @@ export default function Approutes() {
       <Route path="/" element={<HomePage />} />
       <Route path="project/accordion" element={<Accordion/>} />
        <Route path="project/random-color-generator" element={<RandomColorGenerator/>} />
+       <Route path="project/star-rating" element={<StarRatingComp/>} />
       <Route path="*" element={<ErrorPage />} />
     </Routes>
   )
