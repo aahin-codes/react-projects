e3e3093c907494351957143fe0cb3c51a0316c87
this.aahin <aahin2001@gmail.com>
2025-11-18 21:32:58 +0530

    added route for img-slider-component

--- src/routes/approutes.jsx
+++ src/routes/approutes.jsx
@@ -4,6 +4,7 @@ import ErrorPage from "../errorpage/errorpage";
 import Accordion from "../accordion-component/accordion";
 import RandomColorGenerator from "../random-color-generator-component/random-color-generator";
 import StarRatingComp from "../star-rating-component/star-rating-comp";
+import ImgSliderComp from "../image-slider-component/img-slider-comp";
 
 export default function Approutes() {
   return (
@@ -13,6 +14,7 @@ export default function Approutes() {
         <Route path="accordion" element={<Accordion/>} />
         <Route path="random-color-generator" element={<RandomColorGenerator/>} />
         <Route path="star-rating" element={<StarRatingComp/>} />
+        <Route path="image-slider" element={<ImgSliderComp/>} />
       </Route>
       <Route path="*" element={<ErrorPage />} />
     </Routes>
