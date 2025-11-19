08f001c0bee53615b4aa039635532f4acdc92b46
this.aahin <aahin2001@gmail.com>
2025-11-19 21:51:39 +0530

    added route for load-more-component

--- src/routes/approutes.jsx
+++ src/routes/approutes.jsx
@@ -5,6 +5,7 @@ import Accordion from "../accordion-component/accordion";
 import RandomColorGenerator from "../random-color-generator-component/random-color-generator";
 import StarRatingComp from "../star-rating-component/star-rating-comp";
 import ImgSliderComp from "../image-slider-component/img-slider-comp";
+import LoadMoreComponent from "../load-more-component/load-more-component";
 
 export default function Approutes() {
   return (
@@ -15,6 +16,7 @@ export default function Approutes() {
         <Route path="random-color-generator" element={<RandomColorGenerator/>} />
         <Route path="star-rating" element={<StarRatingComp/>} />
         <Route path="image-slider" element={<ImgSliderComp/>} />
+        <Route path="load-more" element={<LoadMoreComponent/>} />
       </Route>
       <Route path="*" element={<ErrorPage />} />
     </Routes>
